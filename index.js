25b652ce42df67d8653dbd02d4f5d6219ae08968
lemuelnjuki <lemuel.njuki@student.moringaschool.com>
2022-08-28 09:44:03 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,39 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  if (require.main === module) {
+    console.log("Expecting: true");
+    console.log("=>", isPalindrome("racecar"));
+  
+    console.log("");
+  
+    console.log("Expecting: true");
+    console.log("=>", isPalindrome("mom"));
+  
+    console.log("");
+  
+    console.log("Expecting: true");
+    console.log("=>", isPalindrome("abba"));
+  
+    console.log("");
+  
+    console.log("Expecting: true");
+    console.log("=>", isPalindrome("a"));
+  
+    console.log("");
+  
+    console.log("Expecting: false");
+    console.log("=>", isPalindrome("hi"));
+  
+    console.log("");
+  
+    console.log("Expecting: false");
+    console.log("=>", isPalindrome("robot"));
+  }
+  let reversedWord = reversedString(word);
+  return word ==reversedWord;
+}
+function reversedString(word){
+  return word.split("").reverse().join("");
 }
 
 /* 
@@ -8,7 +42,11 @@ function isPalindrome(word) {
 
 /*
   Add written explanation of your solution here
+  word.split tturns string ino array
+  .reverse() turns array to reverse form
+  join turns array to string
 */
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
